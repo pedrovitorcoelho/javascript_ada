d06dddf09528cd30f1745356b4da556932803280
Pedro Vitor <pedrovitorcoelho@outlook.com>
2023-07-18 23:42:22 -0300

    finalizando operadores aritméticos em js

--- operadores_aritmeticos.js
+++ operadores_aritmeticos.js
@@ -1,2 +1,41 @@
 // Trabalhando com operações matemáticas.
 
+//Em Java script temos os seguinte operadores;
+
+// adição:            +
+// subtração:         -
+// Multiplicação:     *
+// Divisão:           /
+// Exponenciação:     **
+// Resto da divisão:  %
+
+
+let number1 = 5;
+let number2 = 2
+
+console.log('soma = ', number1 + number2);
+console.log('subtração = ', number1 - number2);
+console.log('multiplicação = ', number1 * number2);
+console.log('Exponenciação = ', number1 ** number2);
+console.log('Resto da divisão = ', number1 % number2);
+
+
+// É importante lembrar que na programação, seja qualquer linguagem, ela segue a ordem de calculo da matematica, ou seja:
+// primeiro multiplicação e divisão e depois subtração e adição 
+
+
+// BIBLIOTECA MATH
+
+// Essa biblioteca é intgrada com js, e traz ferramentas prontas para ajudar na programação
+
+console.log(Math.PI);
+
+console.log(Math.sqrt(9)); // Descobrir a raiz quadrade de um número;
+
+
+//SITE TOP para visitar documentação: MDN JavaScript;
+
+
+
+
+
