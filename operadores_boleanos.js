7419a1ae7d739524e673f56123ba1148ce4a24ad
Pedro Vitor <pedrovitorcoelho@outlook.com>
2023-07-18 23:55:51 -0300

    trabalhando com operadores boleanos

---
+++ operadores_boleanos.js
@@ -0,0 +1,32 @@
+// OPERADORES BOLEANOS EM JS
+
+// igualdade  == (ou ===);
+// Desigualdade != (ou !==)
+// Maior que : >
+// Menor que: <
+// Maior ou igual: >=
+// Menor ou igual: <= 
+
+const num = 1
+
+console.log(num > 2);
+console.log (num >= 1);
+
+
+console.clear(); // Limpar o console.
+
+//  CONJUNÇÕES LÓGICAS
+
+// AND => &&
+// OR => ||
+// NOT => !
+
+let idade = 18
+let cnh = true;
+
+let PoderDirigir = idade >= 18 && cnh === true; // Aqui estamos fazendo uma variável que poder ser true, se cumprir o conteudo que pede nas outras variáveis
+
+console.log(PoderDirigir);
+
+
+
